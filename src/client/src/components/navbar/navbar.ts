90b8e7dc2540652b7d183e4473cc52ce45c91762
tuscan-services-caleb <caleb.smith@tuscanservicesinc.com>
2026-02-07 19:22:29 -0600

    Add .NET API project and setup barebones Angular frontend

---
+++ src/client/src/components/navbar/navbar.ts
@@ -0,0 +1,13 @@
+﻿import {Component} from '@angular/core';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {gameArchiveResearch} from '@ng-icons/game-icons';
+
+@Component({
+  selector: 'navbar',
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ gameArchiveResearch })],
+  templateUrl: 'navbar.html',
+})
+export class NavbarComponent {
+
+}
