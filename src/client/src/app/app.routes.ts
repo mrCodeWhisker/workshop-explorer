c347387cc6ddc201c4a451829eb54646d09295d9
tuscan-services-caleb <caleb.smith@tuscanservicesinc.com>
2026-02-07 19:26:35 -0600

    Update .gitignore and add project scaffolding files

---
+++ src/client/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import {Home} from './home/home';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: "Workshop Explorer",
+    component: Home
+  },
+];
